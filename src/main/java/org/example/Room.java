028326a290fe6d822d62b3ab43730715386b7f6c
Gligor Anastasia <gligorr.anastasia12@gmail.com>
2024-12-27 23:17:43 +0200

    modoficare_room

--- src/main/java/org/example/Room.java
+++ src/main/java/org/example/Room.java
@@ -11,7 +11,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Room{
-
+//adddaddd
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO) //auto incrementareee
     @Column(name="id_room")
